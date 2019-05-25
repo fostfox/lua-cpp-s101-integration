accb6a8937e1c07ca7c568a53e4fd6f524073f00
ArnMips <arn.mips@gmail.com>
2019-05-25 13:46:08 +0300

    upd config file

--- config.h
+++ config.h
@@ -7,29 +7,42 @@
 
 static const auto dateTime = QString::number(QDateTime::currentSecsSinceEpoch());
 namespace filenames {
+const static QString MAP_SET      = "XMLData/MAP_SET/";
 const static QString MAP          = "XMLData/dataset_map.xml";
 const static QString DICT         = "XMLData/S-101FC_1.0.0_20190409.xml";
 const static QString LUA_MAIN     = "lua/main.lua";
 const static QString PORTRAYAL    = dateTime + "-OUTPUT.txt";
 const static QString PROFILE      = dateTime + "-elapsed_time.txt";
 const static QString LOG          = dateTime + "-log.txt";
-}
+} // namespace filenames
 
 
 namespace contextparams {
 using namespace contextparamstype;
 const static QVector<ContextParameter> PARAMS = {
-    { IGNORE_SCAMIN, false },
-    { RADAR_OVERLAY, false },
-    { SAFETY_DEPTH, 10.5 },
+    /// selected safety depth (meters)
+    { SAFETY_DEPTH, 30.0 },
+    /// selected shallow water contour (meters) (optional)
+    { SHALLOW_CONTOUR, 2.0 },
+    ///selected safety contour (meters)
+    { SAFETY_CONTOUR, 30.0 },
+    ///show two depth shades only, safe vs unsafe
+    { TWO_SHADES, true },
+    ///selected deep water contour (meters) (optional)
+    { DEEP_CONTOUR, 30.0 },
+    ///show the shallow diamond pattern in shallow water
+    { SHALLOW_PATTERN, false },
+    ///show isolated dangers in shallow waters
+    { SHOW_ISOLATED_DANGERS_IN_SHALLOW_WATERS, false },
+    ///simplify portrayal of lines
     { PLAIN_BOUNDARIES, false },
-    { SIMPLIFIED_POINTS, true },
-    { SAFETY_CONTOUR, 10.5 },
-    { SHALLOW_CONTOUR, 10.3 },
-    { DEEP_CONTOUR, 10.0 },
-    { SHALLOW_PATTERN, true },
-    { TWO_SHADES, false },
+    ///simplify portrayal of point symbols
+    { SIMPLIFIED_POINTS, false },
+    ///Tells the portrayal whether a RADAR sweep is displayed or not
+    { RADAR_OVERLAY, true },
+    ///Ignore the scaleMinimum attribute
+    { IGNORE_SCAMIN, false },
+    ///Selects which language to use for text (ISO 639-3)
     { NATIONAL_LANGUAGE, "eng" },
-    { SHOW_ISOLATED_DANGERS_IN_SHALLOW_WATERS, true }
 };
-}
+} // namespace contextparams
